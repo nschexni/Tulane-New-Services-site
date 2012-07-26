7941102874996fb3154d85df0784b9d9978ef306
Neal Schexnider <nschexni@tulane.edu>
2012-07-26 18:51:59 -0500

    EOD 20120726

--- application/views/home/home_view_1.php
+++ application/views/home/home_view_1.php
@@ -5,10 +5,46 @@
     
 <div id="homepage_middle_section"><!-- Home Page Middle Section Begin -->       
     <div id="collections_list">
-        <ul>
-            <li><a href="#">Tulane Journals</a></li>
-            <li><a href="#">Tulane Theses &AMP; Dissertations</a></li>
-        </ul>
+        <div id="collections_list_content">
+            <ul>
+                <li>
+                    <div class="collections_list_img">
+                        <img src="<?=base_url()?>assets/img/home/collections_list_img_journals.png" />
+                    </div>
+
+                    <div class="collections_list_text_area">
+
+                        <h3 class="collections_list_title">
+                            Tulane Journals
+                        </h3>
+
+                        <span class="collections_list_description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </span>
+
+                    </div>
+                </li>
+
+                 <li>
+                    <div class="collections_list_img">
+                        <img src="<?=base_url()?>assets/img/home/collections_list_img_journals.png" />
+                    </div>
+
+                    <div class="collections_list_text_area">
+
+                        <h3 class="collections_list_title">
+                            Tulane Theses &amp; Dissertations
+                        </h3>
+
+                        <span class="collections_list_description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </span>
+
+                    </div>
+                </li>
+
+            </ul>
+        </div>
     </div>    
 
     <div id="news_area">
@@ -42,40 +78,47 @@
 
 <div id="homepage_bottom_section"><!-- Home Page Bottom Section Begin -->  
     <div id="column_contact">
-        <h5>contact</h5>
-        <p>We want to help!</p>
-        <ul>
-            <li>phone</li>
-            <li>email</li>
-            <li>address 1</li>
-            <li>address 2</li>
-            <li>city state zip</li>
-        </ul>
+        <div id="column_contact_content">
+            <h5 class="column_contact">contact</h5>
+            <p>We want to help!</p>
+            <ul>
+                <li>phone</li>
+                <li>email</li>
+                <li>address 1</li>
+                <li>address 2</li>
+                <li>city state zip</li>
+            </ul>
+        </div>
     </div>
     
     <div id="column_statistics">
-        <p>statistics info</p>
-        <h6>most popular items:</h6>
-        <ul>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-        </ul>
-        <h6>recent additions:</h6>
-        <ul>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-        </ul>
+        <div id="column_statistics_content">
+            <p>statistics info</p>
+            <h6>most popular items:</h6>
+            <ul>
+                <li>item</li>
+                <li>item</li>
+                <li>item</li>
+            </ul>
+            <h6>recent additions:</h6>
+            <ul>
+                <li>item</li>
+                <li>item</li>
+                <li>item</li>
+            </ul>
+        </div>
     </div>
     
     <div id="column_submission">
-        <p>submission info</p>
-        <ul>
-            <li>theses &amp; dissertations</li>
-            <li>journal</li>
-        </ul>
+        <div id="column_submission_content">
+            <p>submission info</p>
+            <ul>
+                <li>theses &amp; dissertations</li>
+                <li>journal</li>
+            </ul>
+        </div>
     </div>
+    
 </div>    
     
 </div><!-- end: content wrapper -->
