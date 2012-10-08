2dcf647fb0dc9eb2e86e5802964bbf721b130d10
Neal Schexnider <nschexni@tulane.edu>
2012-10-08 17:35:00 -0500

    got some stuff working! EOD 20121008

--- application/models/carousel_model.php
+++ application/models/carousel_model.php
@@ -2,7 +2,7 @@
 
 class Carousel_model extends CI_Model{
     
-
+    //test sql query to return all carousel data
     function getAll(){
       $q = $this->db->query("SELECT * FROM carousel");
         foreach ($q->result() as $row ) {
@@ -10,7 +10,9 @@ class Carousel_model extends CI_Model{
         }
         return $carousel;
     }
-
+    
+    
+    //test sql query to return all carousel data
     function getFull(){
         $q = $this->db->query(
                  "SELECT carousel.title AS story_title
@@ -28,23 +30,7 @@ class Carousel_model extends CI_Model{
                     )
                 LEFT JOIN listitems ON(
                     carousel.id = listitems.carousel_id
-                    )
-                    
-                ");
-        
-                foreach ($q->result() as $row) {
-                    $carousel[] = $row;
-                    }
-                    return $carousel;
-                }
-
-    function getMain(){
-        $q = $this->db->query(
-                 "SELECT carousel.title AS story_title
-		        ,carousel.link AS story_link
-		        ,carousel.img  AS story_image
-                        ,carousel.alt AS story_alt  
-                   FROM carousel      
+                    ) 
                 ");
         
                 foreach ($q->result() as $row) {
@@ -53,15 +39,57 @@ class Carousel_model extends CI_Model{
                     return $carousel;
                 }
                 
-    function carousel_items(){
+    //Gets the Navigation and Images 
+    function getMain(){
+        $q = $this->db->query(
+                 "SELECT carousel.title AS story_title
+		        ,carousel.link AS story_link
+		        ,carousel.img  AS story_image
+                        ,carousel.alt AS story_alt  
+                   FROM carousel      
+                ");
+                $carousel = array(1);
+                foreach ($q->result() as $row) {
+                    $carousel[] = $row;
+                    }
+                    return $carousel;
+                }
+                
+                
+    //Gets the Subtitle information (if exist)
+    function getSub($c_id){
+        $subitem = null;
+        $q = $this->db->query("
+            SELECT subitem.headline AS headline
+            ,subitem.teaser AS teaser
+            ,subitem.subtitle AS subtitle
+            FROM subitem
+            WHERE carousel_id = $c_id
+            ");
+        foreach ($q->result() as $row){
+                $subitem = $row;
+            }
+        return $subitem;
+    }
+    
+    
+    //Build the array for View   
+    function carousel_items(){ 
       $carousel = $this->getMain();
       $items = array();
-        for( $i=0; $i<=2;$i++ ){
-            //$i_string = strval($i);
-            $items[$i]['item_'.$i.'_title'] = $carousel[$i]->story_title;
-            $items[$i]['item_'.$i.'_link'] = $carousel[$i]->story_link;
-            $items[$i]['item_'.$i.'_img'] = $carousel[$i]->story_image;
-            $items[$i]['item_'.$i.'_alt'] = $carousel[$i]->story_alt;
+        for($i=1; $i<=5;$i++){
+            $items[$i]['item_title'] = $carousel[$i]->story_title;
+            $items[$i]['item_link'] = $carousel[$i]->story_link;
+            $items[$i]['item_img'] = $carousel[$i]->story_image;
+            $items[$i]['item_alt'] = $carousel[$i]->story_alt;
+            
+            //check to see if subitems exist 
+            $subitem = $this->getSub($i);
+            if($subitem !== null){
+                $items[$i]['subitem_headline'] = $subitem->headline;
+                $items[$i]['subitem_teaser'] = $subitem->teaser;
+                $items[$i]['subitem_subtitle'] = $subitem->subtitle;
+            }   
            }
        return $items;
     }
--- application/views/carousel/home_carousel.php
+++ application/views/carousel/home_carousel.php
@@ -1,55 +1,108 @@
+<?php /*
+      <? 
+
+    '<pre>'.  
+    print_r($carousel);
+?> */?>
+
+
 <div id="homepage_carousel">
     <div class="shell container-content-main">
         <div class="container_24 clear">
             <div id="coverStoriesContainer">
                 <div class="evo-wrapper">
                     <div class="evo-coverStories">
-                       
+                       <div class="fuseNav clearfix"> 	
                         
                         <!-- fuseNav -->
-                        <? for($i=0;$i<=2;$i++){ ?>
-                        <div class="fuseNav clearfix"> 	
+                        <? for($i=1;$i<=5;$i++){ ?>                      
                             <a style="width: 189px;" 
-                               href="<?= $carousel[$i]['item_'.$i.'_link']; ?>" 
-                               class="first active"> 	
-                                <span class="text"><?=$carousel[$i]['item_'.$i.'_title']; ?></span> 
+                               href="<?= $carousel[$i]['item_link']; ?>" 
+                               <?// if first item - add the first active class
+                                if($i==1){
+                                    print 'class="first active"';
+                                } 
+                               ?>
+                            > 	
+                                <span class="text"><?=$carousel[$i]['item_title']; ?></span> 
                                 <span class="fuse"><span class="activeFuse"></span> </span> 
                             </a> 
-                        </div>
+                        <? } ?>
                         <!-- END: fuseNav -->
-                        
-                        
-                        
-                        
+                        </div>
+
                         <!-- carousel -->
                         <div class="carousel">
-                            
-                        <div class="storyUnit wide index-<?=$i?>"> 
+                        <? for($i=1;$i<=5;$i++){ ?>    
+                        <div class="storyUnit wide index-<?=$i?> 
+                               <?// if first item - add the first active class
+                                if($i==1){
+                                    print 'active';
+                                } 
+                               ?>
+                             "> 
                                 <!-- big image -->
                                 <div class="cvr-main"> 
-                                    <a href="<?= $carousel[$i]['item_'.$i.'_link']; ?>" > 
-                                            <img src="<?= base_url().$carousel[$i]['item_'.$i.'_img']; ?>" 
-                                                 alt="<?= $carousel[$i]['item_'.$i.'_alt']; ?>" />
+                                    <a href="<?= $carousel[$i]['item_link']; ?>" > 
+                                            <img src="<?= base_url().$carousel[$i]['item_img']; ?>" 
+                                                 alt="<?= $carousel[$i]['item_alt']; ?>" />
                                     </a> 
                                 </div>
                                 <!-- END: cvr-main --> 
+                                
+                                
+                                
+                                
+                                
+                                
+                                <? if(isset($carousel[$i]['subitem_headline'])){ ?>
+                                    <!-- sub items in the right -->
+                                <div class="cvr-highlights">
+                                    <div class="cvr-headline lcs-headline">
+                                        <a href="<?= $carousel[$i]['item_link']; ?>">
+                                            <?= $carousel[$i]['subitem_headline']; ?>
+                                        </a>
+                                    </div>
+                                    <div class="cvr-teaser lcs-teaser"><?= $carousel[$i]['subitem_teaser']; ?>
+                                    </div>
+                                    <div class="cvr-highlightsSubTitle"><?= $carousel[$i]['subitem_subtitle']; ?></div>
+<!--                                    <ul class="evo-coverStories-subitems">
+                                        <li class="alpha"> 
+                                            <a href="<?= $carousel['subitem_two_listitems[0]']['link'] ?>">
+                                                <?= $carousel['subitem_two_listitems[0]']['title'] ?>
+                                            </a> 
+                                        </li>
+                                        <li> 
+                                            <a href="<?= $carousel['subitem_two_listitems[1]']['link'] ?>">
+                                                <?= $carousel['subitem_two_listitems[1]']['title'] ?>
+                                            </a> 
+                                        </li>
+                                        <li> 
+                                            <a href="<?= $carousel['subitem_two_listitems[2]']['link'] ?>">
+                                                <?= $carousel['subitem_two_listitems[2]']['title'] ?>
+                                            </a> 
+                                        </li>
+                                        <li> 
+                                            <a href="<?= $carousel['subitem_two_listitems[3]']['link'] ?>">
+                                                <?= $carousel['subitem_two_listitems[3]']['title'] ?>
+                                            </a> 
+                                        </li>
+                                        
+                                    </ul>-->
+<!--                                     END: evo-coverStories-subitems  -->
+                                </div>
+                                <? }?>
+                                
+                                
+                                
+                                
+                                
+                                
+                                
                         </div>
+                        <? } ?>    
                         <!-- END: carousel --> 
                         </div>
-                        <? } ?>
-                        
-
-                        
-                        
-                        
-
-
-                            
-                            
-                            
-
-                            
-                    
                     <!-- END: evo-coverStories --> 
                 </div>
                 <!-- END: evo-wrapper --> 
